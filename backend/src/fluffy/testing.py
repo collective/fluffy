6b04e2087e44db2864e7a512383275d85e215bbf
Steve Piercy <web@stevepiercy.com>
2025-04-09 11:38:53 -0700

    Fix lint see https://github.com/collective/fluffy/actions/runs/14357413181/job/40250020732

--- backend/src/fluffy/testing.py
+++ backend/src/fluffy/testing.py
@@ -1,4 +1,4 @@
-import fluffy  # noQA
+import fluffy
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
